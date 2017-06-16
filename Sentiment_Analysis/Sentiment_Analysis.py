b44e2f4142de8c376e48b9ece4b72b16ac5d40ff
Hirad Emami Alagha <Hirad.alagha@outlook.com>
2017-06-16 19:30:21 +0200

    Classifier Added

--- Sentiment_Analysis/Sentiment_Analysis.py
+++ Sentiment_Analysis/Sentiment_Analysis.py
@@ -7,6 +7,7 @@ from nltk.twitter import Twitter
 from nltk.corpus import stopwords, state_union, movie_reviews
 from nltk.tokenize import PunktSentenceTokenizer
 from nltk.stem import WordNetLemmatizer
+import pickle
 
 import re
 
@@ -25,9 +26,29 @@ def wordTokenize(dat):
 #Returns a List of tokenized words that do not include stop words
 def filterStopWords(dat):
     stop_words =set(stopwords.words("english"))
-    wordlist= wordTokenize(dat)
+    print (stop_words)
+    wordlist= dat
     filtered= [w for w in wordlist if not w in stop_words]
     return filtered
+
+def filterSymbols(dat):
+    dic = ["!", "?",
+           ".", "@",
+           "[", "]",
+           "{", "}",
+           "*", "&",
+           "(", ")",
+           "_","-",
+           ",",'"',
+           "'","...",
+           "--","`",
+           "$","#",
+           "%","^","+","-","/",":",";"
+           ]
+    wordlist = dat
+    filtered = [w for w in wordlist if not w in dic]
+    return filtered
+
 
 
 #reading Movie Reviews that were creadted and assign it to an array
@@ -37,7 +58,7 @@ def readMovieReviews():
     readMe = open('MovieReviews.txt', 'r').readlines()
 
     # Creates a list containing 5 lists, each of 8 items, all set to 0
-    w, h = 2, len(readMe);
+    w, h = 2, len(readMe)
     document = [[0 for x in range(w)] for y in range(h)]
 
     for i in range(0, len(readMe)):
@@ -96,6 +117,32 @@ def lemmatizeThisWord(dat):
     lemmatizer = WordNetLemmatizer()
     return lemmatizer.lemmatize(dat)
 
+def find_features(document):
+    words = set(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+def convertToDocument():
+    w, h = 2, len(movieReviews)
+    doc = [[0 for x in range(w)] for y in range(h)]
+    for i in range(0, len(movieReviews)):
+        cat=movieReviews[i][0]
+        tokenized= wordTokenize(movieReviews[i][1])
+        setTokenized= set(tokenized)
+        doc[i][1]=cat
+        doc[i][0]=setTokenized
+
+    return doc
+
+def saveClassifier():
+    save_classifier = open("naivebayes.pickle", "wb")
+    pickle.dump(classifier, save_classifier)
+    save_classifier.close()
+
+
 if __name__ =="__main__":
     #reading the corpus
     movieReviews=readMovieReviews()
@@ -103,3 +150,44 @@ if __name__ =="__main__":
     custom_Sent_Tagg = train_Punktokenizer()
     sentence_Example = "Hello Lovely World. This is Just to test Our POS Tagger!"
     #identifyNamedEntity(sentence_Example)
+
+    all_words = []
+    for w in movie_reviews.words():
+        all_words.append(w.lower())
+
+    all_words=filterStopWords(all_words)
+    all_words=filterSymbols(all_words)
+
+    all_words=nltk.FreqDist(all_words)
+
+    word_features = [w[0] for w in sorted(all_words.items(), key=lambda (k, v): v, reverse=True)[:5000]]
+
+    #print (word_features)
+   # print ('daster ')
+   # print((movie_reviews.words('neg/cv000_29416.txt')))
+   # print ('daster ')
+   # k=wordTokenize(movieReviews[100][1])
+   # print k
+   # print ('daster ')
+   # print set(k)
+    #print ('daster')
+   # print (find_features(k))
+
+    documents=convertToDocument()
+
+    featuresets = [(  find_features(rev), category) for (rev,category) in documents]
+
+    # set that we'll train our classifier with
+    training_set = featuresets[:1900]
+
+    # set that we'll test against.
+    testing_set = featuresets[1900:]
+
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+    print("Classifier accuracy percent:", (nltk.classify.accuracy(classifier, testing_set)) * 100)
+
+    classifier.show_most_informative_features(15)
+
+    #if you want to save it 
+   # saveClassifier()
